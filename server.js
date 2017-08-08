9ebd43126c6e281592e73ddcb5ecf6f5c130baae
Siobhan Coady <siobhan.coady@gmail.com>
2017-08-08 15:50:37 -0700

    Edit CORS

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(methodOverride());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 mongoose.connect('mongodb://localhost/ReadyForLaunch');
 
